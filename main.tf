bc7205ead0059c2b81da806509e6c63510887d4e
Rodion Gyrbu <fpsoff@outlook.com>
2021-07-27 12:44:22 +0300

    Minor changes
    Fix variables and outputs

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 # Key Pair
 ###########
 resource "opentelekomcloud_compute_keypair_v2" "this" {
-  count = var.create_key_pair ? 1 : 0
+  count = var.create_keypair ? 1 : 0
 
   name       = "${var.prefix}_keypair"
   public_key = var.public_key
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "keypair_name" {
   description = "The Name of the KeyPair"
-  value       = opentelekomcloud_compute_keypair_v2.this.name
+  value       = concat(opentelekomcloud_compute_keypair_v2.this.*.name, [""])[0]
 }
